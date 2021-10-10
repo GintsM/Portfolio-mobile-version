8cf2a0e0f2392537101072e5ab53730516b2f314
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-10-10 20:27:05 +0300

    Removed Code writing errors and adjustet popup window

--- card.js
+++ card.js
@@ -1,15 +1,8 @@
-function Card (title, expierence, image,) {
+function Card(title, expierence, image) {
   this.title = title;
   this.expierence = expierence;
   this.image = image;
-  this.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing\
-    elit, sed do eiusmod tempor incididunt ut labore et dolore \
-    magna aliqua. Sem et tortor consequat id porta nibh. Velit \
-    aliquet sagittis id consectetur. Urna id volutpat lacus laoreet.\
-    Fames ac turpis egestas integer eget aliquet nibh praesent \
-    tristique. Egestas diam in arcu cursus euismod quis viverra \
-    nibh. Nisl rhoncus mattis rhoncus urna neque. Non tellus orci\
-    ac auctor augue mauris augue.';
+  this.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh. Velit aliquet sagittis id consectetur. Urna id volutpat lacus laoreet. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Egestas diam in arcu cursus euismod quis viverra nibh. Nisl rhoncus mattis rhoncus urna neque. Non tellus orci ac auctor augue mauris augue.';
   this.languages = {
     1: 'html',
     2: 'css',
@@ -25,22 +18,21 @@ function Card (title, expierence, image,) {
 }
 
 const card1 = new Card('Tonic', ['CANOPY', 'Back End Dev', '2021'], 'images/Snap1.png');
-const card2 = new Card('MULTI-POST STORIES', ['FACEBOOK','Fullstack Dev', '2021'], 'images/Snap2.png');
+const card2 = new Card('MULTI-POST STORIES', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap2.png');
 const card3 = new Card('Facebook 360', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap3.png');
 const card4 = new Card('UBER NAVIGATION', ['UBER', 'Lead Developer', '2021'], 'images/Snap4.png');
 
 const cardList = [card1, card2, card3, card4];
-const selectorList = [ '#portfolio', 'section.card.ko', 'section.card.ro', 'section.card.last'];
+const selectorList = ['#portfolio', 'section.card.ko', 'section.card.ro', 'section.card.last'];
 
-for (let num = 0; num < cardList.length; num++) {
-
+for (let num = 0; num < cardList.length; num +=1) {
   const popup1 = document.querySelector(selectorList[num]);
   const modalContainer = document.createElement('div');
   popup1.appendChild(modalContainer);
   modalContainer.classList.add('hidden');
   const div = document.createElement('div');
   modalContainer.appendChild(div);
-  div.classList.add('popup');  
+  div.classList.add('popup');
   const h2 = document.createElement('h2');
   div.appendChild(h2);
   h2.innerHTML = cardList[num].title;
