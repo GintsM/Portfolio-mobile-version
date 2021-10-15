8b5e8ab72ca5261a462971dcdc270a773188ed8d
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-10-15 16:33:24 +0300

    Saving values from contact form in LocalStorage using JavaScript (#17)
    * Saving values from contact form in LocalStorage using JavaScript
    
    * Code looks nicer when no linter errors
    
    * Code looks nicer when no linter errors second time

--- js/script.js
+++ js/script.js
@@ -17,12 +17,40 @@ menuLinks.forEach((li) => li.addEventListener('click', showMenu));
 
 const form = document.querySelector('#about > footer > form');
 const warning = document.querySelector('#warning');
+const emailVal = document.getElementById('email');
 
 form.addEventListener('submit', (event) => {
-  const emailVal = document.getElementById('email').value;
-
-  if (emailVal !== emailVal.toLowerCase()) {
+  if (emailVal.value !== emailVal.value.toLowerCase()) {
     event.preventDefault();
     warning.innerHTML = 'Please use only lower case characters for email.';
   }
 });
+
+// Local storage
+
+const contName = document.getElementById('name');
+const contEmail = emailVal;
+const contTextArea = document.getElementById('textarea');
+
+function addToLocalStorage() {
+  const addToLocalStorageArr = {
+    name: contName.value,
+    mail: contEmail.value,
+    text: contTextArea.value,
+  };
+  const contactString = JSON.stringify(addToLocalStorageArr);
+  localStorage.setItem('formdata', contactString);
+}
+
+function setFromLocalStorage() {
+  const getValuesFromStor = JSON.parse(localStorage.getItem('formdata'));
+  contName.value = getValuesFromStor.name;
+  contEmail.value = getValuesFromStor.mail;
+  contTextArea.value = getValuesFromStor.text;
+}
+
+if (localStorage.getItem('formdata')) {
+  setFromLocalStorage();
+}
+
+form.addEventListener('change', addToLocalStorage);
