8d83de7b66682a1e1cda0c9d3d074475a52f1fe7
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-11-12 16:14:38 +0200

    First project To_Do

--- js/card.js
+++ js/card.js
@@ -1,30 +1,34 @@
-function Card(title, expierence, image) {
+function Card(title, expierence, image, text, live, source) {
   this.title = title;
   this.expierence = expierence;
   this.image = image;
-  this.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh. Velit aliquet sagittis id consectetur. Urna id volutpat lacus laoreet. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Egestas diam in arcu cursus euismod quis viverra nibh. Nisl rhoncus mattis rhoncus urna neque. Non tellus orci ac auctor augue mauris augue.';
+  this.text = text;
   this.languages = {
     1: 'html',
     2: 'css',
     3: 'javaScript',
     4: 'github',
     5: 'ruby',
-    6: 'Bootstrap',
+    6: 'webpack',
   };
   this.links = {
     1: 'See Live',
     2: 'See Source',
   };
-  this.toGo = {
-    live: 'https://gintsm.github.io/Portfolio-mobile-version/',
-    source: 'https://github.com/GintsM/Portfolio-mobile-version',
-  };
+  this.toGo = {live, source};
 }
 
-const card1 = new Card('Tonic', ['CANOPY', 'Back End Dev', '2021'], 'images/Snap1.png');
-const card2 = new Card('MULTI-POST STORIES', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap2.png');
-const card3 = new Card('Facebook 360', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap3.png');
-const card4 = new Card('UBER NAVIGATION', ['UBER', 'Lead Developer', '2021'], 'images/Snap4.png');
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh. Velit aliquet sagittis id consectetur. Urna id volutpat lacus laoreet. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Egestas diam in arcu cursus euismod quis viverra nibh. Nisl rhoncus mattis rhoncus urna neque. Non tellus orci ac auctor augue mauris augue.';
+const live = 'https://gintsm.github.io/Portfolio-mobile-version/';
+const source = 'https://github.com/GintsM/Portfolio-mobile-version';
+const firstTasktxt = 'This project was built a simple list of To Do tasks. The functionality - Add task, remove individual task, edit already created task, clear all completed tasks. Web page was built using webpack and served by a webpack dev server. To create best UI I used class with static methods, eventlistener using propogation to give smoother work';
+const firstLive = 'https://gintsm.github.io/To_do_list/dist/';
+const firstSource = 'https://github.com/GintsM/To_do_list';
+
+const card1 = new Card('ToDo List', ['Microverse', 'Front End Dev', '2021'], 'images/todo.png', firstTasktxt, firstLive, firstSource);
+const card2 = new Card('MULTI-POST STORIES', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap2.png', text, live, source);
+const card3 = new Card('Facebook 360', ['FACEBOOK', 'Fullstack Dev', '2021'], 'images/Snap3.png', text, live, source);
+const card4 = new Card('UBER NAVIGATION', ['UBER', 'Lead Developer', '2021'], 'images/Snap4.png', text, live, source);
 
 const cardList = [card1, card2, card3, card4];
 const selectorList = ['#portfolio', 'section.card.ko', 'section.card.ro', 'section.card.last'];
